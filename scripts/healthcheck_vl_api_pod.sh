64bb3b004babbc5ff20ba7b334fb79e683d3d5ac
Yash Chauhan <50273999+yash-chauhan-dev@users.noreply.github.com>
2025-11-19 01:47:13 +0530

    Update health check URL to use proxy endpoint

--- scripts/healthcheck_vl_api_pod.sh
+++ scripts/healthcheck_vl_api_pod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Checking HTTP health endpoint..."
 for i in {1..20}; do
-  STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "http://$VL_API_ENDPOINT:8080/health")
+  STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "http://$VL_API_POD_ID-8080.proxy.runpod.net/health")
 
   echo "Health Check Response: $STATUS_CODE"
 
